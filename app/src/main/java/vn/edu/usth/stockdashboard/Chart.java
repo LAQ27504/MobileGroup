c04a80b8eff083415d5e0894f0c1ad4d78129978
Trần Bảo Nguyên <nguyentb.22bi13342@usth.edu.vn>
2024-09-08 17:27:26 +0700

    4 page

---
+++ app/src/main/java/vn/edu/usth/stockdashboard/Chart.java
@@ -0,0 +1,80 @@
+package vn.edu.usth.stockdashboard;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chart extends Fragment {
+    private RecyclerView recyclerView;
+    private ImageView icon1, icon2, icon3, icon4;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chart, container, false);
+
+
+        icon1 = view.findViewById(R.id.homeicon);
+        icon2 = view.findViewById(R.id.chart);
+        icon3 = view.findViewById(R.id.pay);
+        icon4 = view.findViewById(R.id.profile);
+
+
+        icon1.setOnClickListener(v -> {
+            navigateToFragment(new Home());
+        });
+
+        icon2.setOnClickListener(v -> {
+            navigateToFragment(new Chart());
+        });
+
+        icon3.setOnClickListener(v -> {
+            navigateToFragment(new Wallet());
+        });
+
+        icon4.setOnClickListener(v -> {
+            navigateToFragment(new profile());
+        });
+
+        recyclerView = view.findViewById(R.id.recycle_view);
+        List<item> itemList = new ArrayList<>();
+        itemList.add(new item(R.drawable.amazon, R.drawable.amazon_chart, R.drawable.amazon_percent));
+        itemList.add(new item(R.drawable.msft, R.drawable.msft_chart, R.drawable.msft_percent));
+        itemList.add(new item(R.drawable.tesla, R.drawable.tesla_chart, R.drawable.tesla_percent));
+        itemList.add(new item(R.drawable.spot, R.drawable.spotchart, R.drawable.spotpercent));
+        itemList.add(new item(R.drawable.nflx, R.drawable.nflxchart, R.drawable.nflxpercent));
+
+
+        MyAdapter2 myAdapter = new MyAdapter2(itemList);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+
+        return view;
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
+}
---
+++ app/src/main/java/vn/edu/usth/stockdashboard/Home.java
@@ -0,0 +1,77 @@
+package vn.edu.usth.stockdashboard;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends Fragment {
+    private RecyclerView recyclerView;
+    private ImageView icon1, icon2, icon3, icon4;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        icon1 = view.findViewById(R.id.homeicon);
+        icon2 = view.findViewById(R.id.chart);
+        icon3 = view.findViewById(R.id.pay);
+        icon4 = view.findViewById(R.id.profile);
+
+
+        icon1.setOnClickListener(v -> {
+            navigateToFragment(new Home());
+        });
+
+        icon2.setOnClickListener(v -> {
+            navigateToFragment(new Chart());
+        });
+
+        icon3.setOnClickListener(v -> {
+            navigateToFragment(new Wallet());
+        });
+
+        icon4.setOnClickListener(v -> {
+            navigateToFragment(new profile());
+        });
+
+        recyclerView = view.findViewById(R.id.recycle_view);  // Use the view object to find the RecyclerView
+        List<item> itemList = new ArrayList<>();
+        itemList.add(new item(R.drawable.amazon, R.drawable.amazon_chart, R.drawable.amazon_percent));
+        itemList.add(new item(R.drawable.msft, R.drawable.msft_chart, R.drawable.msft_percent));
+        itemList.add(new item(R.drawable.tesla, R.drawable.tesla_chart, R.drawable.tesla_percent));
+
+        MyAdapter myAdapter = new MyAdapter(itemList);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+
+        return view;
+    }
+
+    private void navigateToFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
+}
